fe06f0338abe558e84a5e20d31238706f6ab2bf0
mberrouk <mberrouk@student.1337.ma>
2024-01-21 16:59:31 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
-file { '/etc/ssh/ssh_config':
+file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  content   => "
-    Host *
-      SendEnv LANG LC_*
-      HashKnownHosts yes
-      GSSAPIAuthentication yes
-      PasswordAuthentication no
-      IdentityFile ~/.ssh/school
-  "
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    Declare identity file',
 }
