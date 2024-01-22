8bed5218e7ceeb50fa778adccd4df11427a2c978
mberrouk <mberrouk@student.1337.ma>
2024-01-21 17:03:40 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
-file_line { 'Turn off passwd auth':
+file { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line { 'Declare identity file':
+file { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    Declare identity file',
+  line   => '    IdentityFile ~/.ssh/school',
 }
