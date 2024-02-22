8cd01b48b8611a6ce1fff230f2f63cc5fb40584d
mberrouk <mberrouk@student.1337.ma>
2024-02-22 02:03:04 +0100

    init

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
