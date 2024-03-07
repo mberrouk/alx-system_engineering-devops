1fb7f189430ecc29f20cda035af08509055767ca
mberrouk <mberrouk@student.1337.ma>
2024-03-07 22:42:36 +0100

    header

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,10 @@ import requests
 def top_ten(subreddit):
     """Print the top 10 hot posts or print None"""
 
-    headers = {"User-Agent": "linux:alxapi:v1.0 (by /u/PitchSufficient4470)"}
+    headers = {"User-Agent": "linux:Alxapi:v1.0 (by /u/PitchSufficient4470)"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     res = requests.get(url, headers, allow_redirects=False)
-    if res.status_code >= 300:
+    if res.status_code != 200:
         print('None')
     else:
         [print(child.get("data").get("title"))
