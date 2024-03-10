80d55d1c440ac94a282b01086ccce24c6865b65e
mberrouk <mberrouk@student.1337.ma>
2024-03-10 23:20:00 +0000

    update path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # fixe the wp-settings.php
 
 exec { 'wp-set':
-	command: 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path: '/bin/'
+	command =>'/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 }
