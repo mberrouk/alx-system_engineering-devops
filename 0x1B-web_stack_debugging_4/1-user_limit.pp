92d5691fc5f51852d0ae18ebdd72672daafd5597
mberrouk <mberrouk@student.1337.ma>
2024-03-14 22:37:04 +0000

    increase the soft limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,6 @@
 
 # Increase hard and soft file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i -e "/holberton hard/s/5/20/" -e "/holberton hard/s/4/20/" /etc/security/limits.conf',
+  command => 'sed -i -e "/holberton/s/5/20/" -e "/holberton/s/4/20/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
