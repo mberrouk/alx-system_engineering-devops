c1ed2efcc23ab2979fc7f239db3ee33d299ec5fb
mberrouk <mberrouk@student.1337.ma>
2024-03-14 21:03:38 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # Increase the number of open file for nginx service 
-
 exec {'fix--for-nginx':
-	command => "/bin/sed -i 's/15/1000/g' /etc/default/nginx && /usr/bin/env service nginx restart"
+	command => "/usr/bin/env sed -i 's/15/1000/g' /etc/default/nginx && /usr/bin/env service nginx restart"
 }
